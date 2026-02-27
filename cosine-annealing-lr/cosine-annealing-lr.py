6b0755ebac9120a30fda00816e61d1c8df598a7f
Nguyễn Quốc Huy <145661313+ngquochuy0101@users.noreply.github.com>
2026-02-27 13:55:11 +0700

    Add solution for cosine-annealing-lr

---
+++ cosine-annealing-lr/cosine-annealing-lr.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def cosine_annealing_schedule(base_lr, min_lr, total_steps, current_step):
+    """
+    Compute the learning rate using cosine annealing.
+    """
+    # Write code here
+    if current_step == 0:
+        return float(base_lr)
+    elif current_step == total_steps:
+        return float(min_lr)
+    else:
+        lr = min_lr + (1/2) * (base_lr - min_lr) * (1 + np.cos( (np.pi * current_step ) /(total_steps) ))
+        return float(lr)
