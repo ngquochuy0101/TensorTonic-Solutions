6cfc6feb6dbfe65335ca02bf91314a739bb0811e
Nguyễn Quốc Huy <145661313+ngquochuy0101@users.noreply.github.com>
2026-03-02 14:32:43 +0700

    Update solution for percentiles

--- percentiles/percentiles.py
+++ percentiles/percentiles.py
@@ -6,19 +6,18 @@ def percentiles(x, p):
     Compute percentiles using linear interpolation.
     """
     # Write code here
-    n = len(x)
+    n = len (x)
     x = np.array(x)
-    x = np.sort(x)
     p = np.array(p)
+    x = np.sort(x)
     
-    index = p/100 * (n-1)
+    index = (p/100) * (n - 1)
+
     low = np.floor(index).astype(int)
     high = np.ceil(index).astype(int)
-    fraction = index - low
-    
-    result = x[low] + (x[high] - x[low]) * fraction
+    fration = index - low
+
+    result = x[low] + (x[high] - x[low])*fration
+
     return result
-    
-x = [4,1,3,2]
-q =[25,75]
-print(percentiles(x,q))
+
