b2bd734149110ce87f47e5152b96790ca2ea4203
Nguyễn Quốc Huy <145661313+ngquochuy0101@users.noreply.github.com>
2026-03-04 08:53:13 +0700

    Update solution for log-transform

--- log-transform/log-transform.py
+++ log-transform/log-transform.py
@@ -1,13 +1,12 @@
 import numpy as np
+import numpy as np
 def log_transform(values):
     """
     Apply the log1p transformation to each value.
     """
     
-    values = np.array(values)
-    values = np.maximum(0, values)
-    
-    y = np.log(1 + values)
-    
-    y = y.tolist()
-    return y
+
+    return np.log1p(np.maximum(0,np.array(values))).tolist()
+                    
+values = [-1,1,2,3]
+print(log_transform(values))
