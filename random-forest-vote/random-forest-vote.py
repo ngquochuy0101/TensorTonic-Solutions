90f659ee8c6bdf64ea99b5664d5a07c948cb52bc
Nguyễn Quốc Huy <145661313+ngquochuy0101@users.noreply.github.com>
2026-03-16 15:47:54 +0700

    Update solution for random-forest-vote

--- random-forest-vote/random-forest-vote.py
+++ random-forest-vote/random-forest-vote.py
@@ -1,9 +1,11 @@
-import numpy as np
 from scipy import stats
 def random_forest_vote(predictions):
     """
     Compute the majority vote from multiple tree predictions.
     """
     # Write code here
-    mode, count = stats.mode(predictions)
+    mode, _ = stats.mode(predictions)
     return mode.tolist()
+    
+predictions = [[0,1,2]]
+print(random_forest_vote(predictions))
