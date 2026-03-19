25ef033fa4b631103f6db68865a431b835ff0cde
Nguyễn Quốc Huy <145661313+ngquochuy0101@users.noreply.github.com>
2026-03-19 14:03:55 +0700

    Update solution for kfold-split

--- kfold-split/kfold-split.py
+++ kfold-split/kfold-split.py
@@ -1,11 +1,5 @@
 import numpy as np
-
 def kfold_split(N, k, shuffle=True, rng=None):
-    """
-    Returns: list of length k with tuples (train_idx, val_idx)
-    """
-    # Write code here
-
     if shuffle == True:
         if rng is not None:
             arr = rng.permutation(N)
